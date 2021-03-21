c8870969146c5bb87a72d6752b96eccaa5e0a68b
Michal Wrona <wronamichal01@gmail.com>
2021-03-21 23:32:05 +0100

    Correct Sing Up and add flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, request
-import jsonpickle
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -12,4 +11,23 @@ def sing_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        if len(first_name) < 2:
+            flash("First name must be at least 3 characters.", category="error")
+        elif len(surname) < 2:
+            flash("Surname must be at least 3 characters.", category="error")
+        elif len(email) < 3:
+            flash("Email must be at least 4 characters.", category="error")
+        elif len(password1) < 6:
+            flash("Password must be at least 7 characters.", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match.", category="error")
+        else:
+            flash("You have been registered.", category="success")
+
     return render_template("sing_up.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return render_template("login.html")
