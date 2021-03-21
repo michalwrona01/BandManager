d5d34566f216afde05c952c0aad3f2fb3ccb14dc
Michal Wrona <wronamichal01@gmail.com>
2021-03-21 14:40:08 +0100

    Add auth.route 'sing_up' method.

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import jsonpickle
 
 auth = Blueprint("auth", __name__)
 
 @auth.route('/sing-up', methods=['GET', 'POST'])
 def sing_up():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        surname = request.form.get('surname')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template("sing_up.html")
