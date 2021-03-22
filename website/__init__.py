80c03e5447504118839811e530a0b1967ddf08db
Michal Wrona <wronamichal01@gmail.com>
2021-03-22 09:52:54 +0100

    Add connect and insert users to database.

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+import mysql.connector
+
+def connection_with_data_base():
+    connection = mysql.connector.connect(
+        database="bandmanage",
+        auth_plugin="mysql_native_password",
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="",
+    )
+    return connection
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'xdxdxd'
+
+    connection = connection_with_data_base()
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, flash
+from . import connection_with_data_base
+import mysql.connector
 
 auth = Blueprint("auth", __name__)
 
@@ -22,7 +24,23 @@ def sing_up():
         elif password1 != password2:
             flash("Passwords don't match.", category="error")
         else:
-            flash("You have been registered.", category="success")
+            try:
+                connection = connection_with_data_base()
+                cursor = connection.cursor()
+                insertQuery = "INSERT INTO bandmanage.users(first_name, surname, email, password) VALUES(%(first_name)s, %(surname)s, %(email)s, %(password)s)"
+                insertData = {
+                    "first_name" : first_name,
+                    "surname" : surname,
+                    "email" : email,
+                    "password" : password1
+                }
+                cursor.execute(insertQuery, insertData)
+                connection.commit()
+                flash("You have been registered.", category="success")
+            except mysql.connector.errors.IntegrityError:
+                flash("You are already registered.", category="error")
+            finally:
+                connection.close()
 
     return render_template("sing_up.html")
 
