a4672e0244cd8dc87b35123436703e18f58b9419
Michal Wrona <wronamichal01@gmail.com>
2021-03-22 22:21:11 +0100

    Add security password and initially 'Create New Band' page.

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, request, flash
-from mysql.connector import cursor
 from .models import connection_with_data_base
 import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
+
 
 @auth.route('/sing-up', methods=['GET', 'POST'])
 def sing_up():
@@ -26,14 +27,16 @@ def sing_up():
             flash("Passwords don't match.", category="error")
         else:
             try:
+                hashed_password = generate_password_hash(
+                    password1, method='sha256')
                 connection = connection_with_data_base()
                 cursor = connection.cursor()
                 insertQuery = "INSERT INTO bandmanage.users(first_name, surname, email, password) VALUES(%(first_name)s, %(surname)s, %(email)s, %(password)s)"
                 insertData = {
-                    "first_name" : first_name,
-                    "surname" : surname,
-                    "email" : email,
-                    "password" : password1
+                    "first_name": first_name,
+                    "surname": surname,
+                    "email": email,
+                    "password": hashed_password
                 }
                 cursor.execute(insertQuery, insertData)
                 connection.commit()
@@ -44,6 +47,7 @@ def sing_up():
                 connection.close()
 
     return render_template("sing_up.html")
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -60,15 +64,13 @@ def login():
         for row in cursor:
             if row["email"] == email:
                 is_exists = False
-                if row["password"] == password: 
+                if check_password_hash(row['password'], password):
                     flash("You just have been logged.", category="success")
                     break
                 else:
-                    flash("Inccorect password, try again.", category="error")    
-        
-        if is_exists: 
-            flash("Account don't exist.", category="error") 
+                    flash("Inccorect password, try again.", category="error")
 
-
+        if is_exists:
+            flash("Account don't exist.", category="error")
 
     return render_template("login.html")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
+from flask.globals import request
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/create-new-band', methods=['GET', 'POST'])
+def create_new_band():
+    data = request.form.get
+    print(data)
+
+    return render_template('create_new_band.html')
