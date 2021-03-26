e24bd4bdad267931d7335972441ded601d7b24b9
Michal Wrona <wronamichal01@gmail.com>
2021-03-26 16:56:21 +0100

    "Add adding new members to music band and add delete members from admin band. Add first perrmisions."

--- website/views.py
+++ website/views.py
@@ -44,18 +44,59 @@ def create_new_band():
 @views.route('/manage-your-band', methods=['GET'])
 @login_required
 def manage_your_band():
-    bands = Band.query.filter_by(user_id_admin=current_user.id).all()
+    bands = Band.query.filter_by(id=current_user.band_id).all()
+    bands_admin = Band.query.filter_by(user_id_admin=current_user.id).all()
 
-    return render_template('manage_your_band.html', user=current_user, bands=bands)
+    if bands == [] and bands_admin == []:
+        return render_template('manage_your_band.html', user=current_user, bands=[])
+    elif bands == []:
+        return render_template('manage_your_band.html', user=current_user, bands=bands_admin)
+    else:
+        return render_template('manage_your_band.html', user=current_user, bands=bands)
 
-@views.route('/bands', methods=['GET', 'POST'])
+    
+
+@views.route('/manage-your-band/<int:band_id>', methods=['GET', 'POST'])
 @login_required
-def band_manager():
-    band_id = request.form.get('band')
+def band_manager(band_id):
     band = Band.query.filter_by(id=band_id).first()
-    users = User.query.filter_by().all()
+    users_in_this_band = User.query.filter_by(band_id=band.id)
+    users_without_band = User.query.filter_by(band_id=None).all()
+    list_with_index_and_user_object = []
+    index = 1
 
-    return render_template('band_manager.html', user=current_user, band=band, users=users)
+    for user in users_in_this_band:
+        list_with_index_and_user_object.append([index, user])
+        index += 1
+
+
+
+    return render_template('band_manager.html', user=current_user, band=band, 
+                            users_in_this_band=list_with_index_and_user_object, users_without_band=users_without_band)
+
+@views.route('/manage-your-band/add/<int:user_id>', methods=['POST'])
+@login_required
+def add_new_member(user_id):
+    if request.method == "POST":
+        user = User.query.filter_by(id=user_id).first()
+        user.band_id = current_user.band_id
+        db.session.commit()
+
+    band = Band.query.filter_by(user_id_admin=current_user.id).first()
+    return redirect(url_for('views.band_manager', band_id=band.id))
+
+@views.route('/manage-your-band/delete/<int:user_id>', methods=['POST'])
+@login_required
+def delete_member(user_id):
+    if request.method == "POST":
+        user = User.query.filter_by(id=user_id).first()
+        user.band_id = None
+        db.session.commit()
+
+    band = Band.query.filter_by(user_id_admin=current_user.id).first()
+    
+    return redirect(url_for('views.band_manager', band_id=band.id))
+
 
 @views.route('/community', methods=['GET'])
 @login_required
